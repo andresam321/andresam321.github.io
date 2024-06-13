3209bde1d8256b57abb6a7d1cba03172de5bb2ec
Andres Mercado <andresmercado96@gmail.com>
2024-06-13 16:45:45 -0700

    Prepare for Netlify deployment

--- tunis/src/router/AllRoutes.js
+++ tunis/src/router/AllRoutes.js
@@ -1,15 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Preview from "../views/Preview";
 import HomeDark from "../views/all-home-version/HomeDark";
 import HomeRtl from "../views/all-home-version/HomeRtl";
 import NotFound from "../views/NotFound";
-import { Routes, Route } from "react-router-dom";
 import ContextProvider from "../Context/ContextProvider";
 import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
 
 const AllRoutes = () => {
   return (
-    <>
+    <Router basename={process.env.PUBLIC_URL}>
       <ContextProvider>
         <ScrollTopBehaviour />
         <Routes>
@@ -19,7 +19,7 @@ const AllRoutes = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </ContextProvider>
-    </>
+    </Router>
   );
 };
 
