e7353f5b952b5a8eb6fcc3b04581d45f693ed450
Andres Mercado <andresmercado96@gmail.com>
2024-06-13 18:04:29 -0700

    try08

--- tunis/src/router/AllRoutes.js
+++ tunis/src/router/AllRoutes.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Preview from "../views/Preview";
 import HomeDark from "../views/all-home-version/HomeDark";
 import HomeRtl from "../views/all-home-version/HomeRtl";
 import NotFound from "../views/NotFound";
+import { Routes, Route } from "react-router-dom";
 import ContextProvider from "../Context/ContextProvider";
 import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
 
 const AllRoutes = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <>
       <ContextProvider>
         <ScrollTopBehaviour />
         <Routes>
           <Route path="/" element={<HomeDark />} />
-          <Route path="/home-dark" element={<HomeDark />} /> 
-          <Route path="/home-rtl" element={<HomeRtl />} /> 
+          {/* <Route path="/home-dark" element={<HomeDark />} /> */}
+          {/* <Route path="/home-rtl" element={<HomeRtl />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </ContextProvider>
-    </Router>
+    </>
   );
 };
 
